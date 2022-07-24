f2cacef702667867abaacafb25fecc76af25ad81
karenm.hirayama <karenm.hirayama@gmail.com>
2022-07-24 12:15:18 -0300

    Change getProfit controller

--- src/controllers/bitcoin.ts
+++ src/controllers/bitcoin.ts
@@ -12,6 +12,7 @@ export const getHistoricalPrice = async (req, res) => {
 };
 
 export const getProfit = async (req, res) => {
+    const {initial_date, final_date, investiment_value} = req.query;
     try {
         const profit = 0;
         res.status(200).json(profit);

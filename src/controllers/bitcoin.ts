12db5a5bebf4893dc6569b64d07aa06bc3366d6b
karenm.hirayama <karenm.hirayama@gmail.com>
2022-07-24 17:47:50 -0300

    Change controllers

--- src/controllers/bitcoin.ts
+++ src/controllers/bitcoin.ts
@@ -1,23 +1,20 @@
-import express from 'express';
+import { Response, Request } from 'express';
 import mongoose from 'mongoose';
 import Bitcoin from '../models/bitcoin';
 
-export const getHistoricalPrice = async (req, res) => {
-    const { date } = req.query;
-    try {
-        const historicalPrice = await Bitcoin.find();
-        res.status(200).json(historicalPrice);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    };
+export const getHistoricalPrice = async (request: Request, response: Response) => {
+    console.log(request.file?.buffer.toString("utf-8"));
+    return response.send();
 };
 
-export const getProfit = async (req, res) => {
-    const { initial_date, final_date, investiment_value } = req.query;
+export const getProfit = async (request: Request, response: Response) => {
+    const { initial_date, final_date, investiment_value } = request.query;
     try {
+        console.log('Sucess in getProfit');
         const profit = 0;
-        res.status(200).json(profit);
+        response.status(200).json(profit);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        response.status(404).json({ message: error.message });
+        console.log('Error in getProfit');
     };
 };

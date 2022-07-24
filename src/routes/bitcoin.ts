0eac4c1e950e593ccb295c1e501b0ef6284cd0af
karenm.hirayama <karenm.hirayama@gmail.com>
2022-07-24 19:00:49 -0300

    Update package json

--- src/routes/bitcoin.ts
+++ src/routes/bitcoin.ts
@@ -1,12 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { getHistoricalPrice, getProfit } from '../controllers/bitcoin';
-import multer from 'multer';
 
 const router = Router();
 
-const multerConfig = multer();
-
-router.post('/', multerConfig.single('file'), getHistoricalPrice)
+router.post('/', getHistoricalPrice)
 router.get('/rentability', getProfit);
 
 export default router;

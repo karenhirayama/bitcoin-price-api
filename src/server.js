7e8c9cb1e7cd64634f9621f2a3ae09ee3eff6e30
karenhirayama <karenm.hirayama@gmail.com>
2022-07-31 18:59:37 -0300

    Create server file

--- src/server.js
+++ src/server.js
@@ -1,2 +1,11 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const needle = require("needle");
 
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.CONNECTION_URL, {
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))

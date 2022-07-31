be247c986d1b84ea0818f98291482cb8568d11ad
karenhirayama <karenm.hirayama@gmail.com>
2022-07-31 18:02:09 -0300

    Delete

--- src/routes/bitcoin.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getHistoricalPrice, getProfit } from '../controllers/bitcoin';
-
-const router = Router();
-
-router.post('/', getHistoricalPrice)
-router.get('/rentability', getProfit);
-
-export default router;
